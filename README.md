fd0d9528ff754dd1ef08264aa96e247b56a3c0c1
danieljuniorce <danieljuniorce@hotmail.com>
2021-09-04 01:54:13 -0300

    add file readme.md and delete

---
+++ README.md
@@ -0,0 +1,16 @@
+![logo-horizontal](https://user-images.githubusercontent.com/33224319/132082662-1ee95904-40bd-4a98-91a0-3765ae646a8f.png)
+
+## Database API - Cayman Comunity
+> API com funções prontas estilo ORM para facilitar o uso do CRUD com o utilizando as functions do dbConnect do MTA:SA.
+
+```lua
+
+### Selecionar
+
+### Inserir
+
+### Atualizar
+
+### Deletar
+
+````
--- global.lua
+++ global.lua
@@ -10,6 +10,44 @@ function Database:Connect(database, host, username, password)
   return self.access
 end
 
+function Database:Select(tableDb, params)
+  local v = {}
+  local c = {}
+
+  if type(tableDb) == "string" and type(params) == "table" then
+    for index, values in pairs(params) do
+      if index == "verify" then
+        table.insert(v, values)
+      elseif index == "condition" then
+        table.insert(c, values)
+      end
+    end
+
+    local wh = ""
+    local par = {}
+    for index, values in pairs(v[1]) do
+      index = index .. " = ? "
+
+      for i, v in pairs(c[1]) do
+        index = index .. v .. " "
+        break
+      end
+      wh = wh .. index
+      table.insert(par, values)
+    end
+
+    local send = self.access:query("SELECT * FROM " .. tableDb .. " WHERE " .. wh:sub(1, -6), unpack(par)):poll(-1)
+
+    if send then
+      return send
+    end
+
+    return false
+  end
+
+  return false
+end
+
 function Database:Insert(tableDb, params)
   local d = ""
   local v = {}
@@ -24,7 +62,6 @@ function Database:Insert(tableDb, params)
 
     local send = self.access:exec("INSERT INTO " .. tableDb .. " SET " .. d:sub(1, -3), unpack(v))
     if not send then
-      outputDebugString("Error: " .. send)
       return false
     end
     return true
@@ -76,7 +113,6 @@ function Database:Update(tableDb, params)
       local send =
         self.access:exec("UPDATE " .. tableDb .. " SET " .. up:sub(1, -3) .. " WHERE " .. wh:sub(1, -6), unpack(par))
       if not send then
-        outputDebugString("Error: " .. send)
         return false
       end
       return true
@@ -86,26 +122,62 @@ function Database:Update(tableDb, params)
   return false
 end
 
-function Database:Delete(table, params)
-  if type(table) == "string" then
-    outputDebugString(inspect(self))
+function Database:Delete(tableDb, params)
+  local v = {}
+  local c = {}
+
+  if type(tableDb) == "string" and type(params) == "table" then
+    for index, values in pairs(params) do
+      if index == "verify" then
+        table.insert(v, values)
+      elseif index == "condition" then
+        table.insert(c, values)
+      end
+    end
+
+    local wh = ""
+    local par = {}
+    for index, values in pairs(v[1]) do
+      index = index .. " = ? "
+
+      for i, v in pairs(c[1]) do
+        index = index .. v .. " "
+        break
+      end
+      wh = wh .. index
+      table.insert(par, values)
+    end
+
+    local send = self.access:exec("DELETE FROM " .. tableDb .. " WHERE " .. wh:sub(1, -6), unpack(par))
+
+    if not send then
+      return false
+    end
+    return true
   end
+
+  return false
 end
 
-function Database:Select(table)
-end
+function Database:Raw(query)
+  if type(query) == "string" then
+    local send = self.access:query(tostring(query)):poll(-1)
 
-function Database:Raw(table, query)
+    if send then
+      outputDebugString(inspect(send))
+
+      return send
+    end
+
+    return false
+  end
+
+  return false
 end
 
 Database:Connect("ccr_database", "127.0.0.1", "root", "")
+--Database:Delete("logs", {verify = {log_responsible = 1}, condition = {"AND"}})
+--Database:Raw("SELECT * FROM logs")
+--[[Database:Select("logs", {verify = {log_responsible = 1, log_type = "Car"}, condition = {"AND", "AND"}})]]
 --Database:Insert("logs", {log_responsible = 1, log_type = "Car", log_action = "Buy", log_create = "2021-09-03 16:48:40"})
-
---[[Database:Update(
-  "logs",
-  {
-    params = {log_action = "Sell for users(1)"},
-    verify = {log_responsible = 1, log_type = "Car"},
-    condition = {"AND", "AND"}
-  }
-)]]
+--[[Database:Update("logs", {params = {log_action = "Sell for users(1)"}, verify = log_responsible = 1, log_type = "Car"}, condition = {"AND", "AND"}}))]]
